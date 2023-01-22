62770ca4269608978df1e9bba2d75c337fdcc985
Luis Mercado <luisbzmercado1776@gmail.com>
2023-01-22 16:18:55 +0800

    update supported types/output types

--- octoprint_ext_sensor_mgr/sensor/sensor_out_type.py
+++ octoprint_ext_sensor_mgr/sensor/sensor_out_type.py
@@ -3,6 +3,7 @@ from enum import Flag, auto
 class SensorOutputType(Flag):
     TEMPERATURE = auto()
     HUMIDITY = auto()
+    PRESSURE = auto()
     PM1_0 = auto()
     PM2_5 = auto()
     PM10 = auto()
--- octoprint_ext_sensor_mgr/sensor/sensor_type.py
+++ octoprint_ext_sensor_mgr/sensor/sensor_type.py
@@ -5,6 +5,9 @@ class SensorType(Enum):
     NOT_SET = -1
     DHT22 = 1
     PMS7003 = 2
+    SHT30 = 3
+    QMP6988 = 4
+    ENV3_UNIT = 5
     
 
 SUPPORTED_SENSOR_LIST = [s for s in SensorType]
